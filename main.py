6ed3f4b79fe3f9175f260076be6e5e07efdf260e
TH3AL3X <themachinehack@gmail.com>
2023-02-28 19:21:37 +0100

    ,

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import menus.menu, base_de_datos.database, os
+import menus.menu, base_de_datos.database, os, menus.menu_grafico
 
 
 base_de_datos.database.Usuarios.cargar_usuarios() # cargamos el json y lo pasamos a la clase con un for
@@ -7,4 +7,5 @@ base_de_datos.database.Usuarios.cargar_historial_usuarios()
 base_de_datos.database.Gerentes.cargar_restaurantes()
 base_de_datos.database.Gerentes.cargar_historial_restaurantes()
 
-menus.menu.menu_principal() # cargamos el menu
+menus.menu_grafico.menu_principal_grafico_inicio_sesion()
+#menus.menu.menu_principal() # cargamos el menu
